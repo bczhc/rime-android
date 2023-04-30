ba2721b4cc8f8662d7efbe0dd1272b64e45f8263
bczhc <bczhc0@126.com>
2023-04-30 21:48:08 +0800

    init commit

---
+++ app/src/main/java/pers/zhc/android/rime/MainActivity.kt
@@ -0,0 +1,11 @@
+package pers.zhc.android.rime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
