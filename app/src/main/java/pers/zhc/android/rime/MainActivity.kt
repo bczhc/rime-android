309114ef65f7ca375278d2ee5877b9df0e5f3bf8
Zhai Can <bczhc0@126.com>
2024-04-27 05:15:07 +0800

    add permission checking

--- app/src/main/java/pers/zhc/android/rime/MainActivity.kt
+++ app/src/main/java/pers/zhc/android/rime/MainActivity.kt
@@ -2,12 +2,16 @@ package pers.zhc.android.rime
 
 import android.Manifest
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import pers.zhc.android.rime.databinding.ActivityMainBinding
 import pers.zhc.android.rime.rime.JNI
+import pers.zhc.android.rime.util.AllFilesAccessPermissionRequestContract
 import pers.zhc.android.rime.util.ToastUtils
 import pers.zhc.tools.utils.RecyclerViewUtils
 import pers.zhc.tools.utils.addDividerLines
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkPermission()
         val bindings = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindings.root)
 
@@ -65,6 +70,36 @@ class MainActivity : AppCompatActivity() {
                     startActivity(Intent(this, KeyTestActivity::class.java))
                 }
             }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private val requestAllFilesAccessLauncher = registerForActivityResult(
+        AllFilesAccessPermissionRequestContract()
+    ) {
+        if (Environment.isExternalStorageManager()) {
+            // permission granted
+        } else {
+            ToastUtils.show(this, R.string.please_grant_permission)
+            finish()
+        }
+    }
+    private val generalRequestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+    }
+
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // after API 30, request "all files access" permission
+            if (Environment.isExternalStorageManager()) {
+                // granted
+            } else {
+                requestAllFilesAccessLauncher.launch(null)
+            }
+        } else {
+            // legacy storage permission
+            generalRequestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
     }
 }
---
+++ app/src/main/java/pers/zhc/android/rime/util/AllFilesAccessPermissionRequestContract.kt
@@ -0,0 +1,30 @@
+package pers.zhc.android.rime.util
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.annotation.RequiresApi
+import pers.zhc.android.rime.BuildConfig
+
+/**
+ * input: package
+ */
+@RequiresApi(Build.VERSION_CODES.R)
+class AllFilesAccessPermissionRequestContract : ActivityResultContract<String?, Unit>() {
+    override fun createIntent(context: Context, input: String?): Intent {
+        return if (input != null) {
+            Intent(
+                Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+                Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+            )
+        } else {
+            Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+        }
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?) {
+    }
+}
